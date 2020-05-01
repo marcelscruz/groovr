79d8acfbf6558acdef4a03a368ca5b78ebfa114b
Marcel <marcelcruz@live.com>
2020-05-01 12:41:41 -0300

    Add assets

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kGrey1 = Color(0xFFBFBFBF);
+const kGrey2 = Color(0xFF8F9090);
+const kGreyBackground = Color(0xFF1E1E1E);
+const kYellow = Color(0xFFF0A500);
+const kWhite = Color(0xFFFFFFFF);
+const kBlack = Color(0xFF000000);
