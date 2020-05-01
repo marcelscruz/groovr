1c95cb03c0e719a2968d1efd4e9b2d1e480be43f
Marcel <marcelcruz@live.com>
2020-05-01 15:43:32 -0300

    Add sliding panel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,12 +16,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final PanelController _pc = new PanelController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        child: Text('Agora vai!'),
+        child: SlidingUpPanel(
+          controller: _pc,
+          panel: Center(
+            child: Text('Menu here!'),
+          ),
+          header: BottomNavBar(
+            panelController: _pc,
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Text('Hey'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  final PanelController panelController;
+
+  BottomNavBar({@required this.panelController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        child: Text(
+          'Toggle Menu',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        onPressed: () {
+          print('toggle menu');
+          panelController.isPanelClosed
+              ? panelController.open()
+              : panelController.close();
+        },
       ),
     );
   }
