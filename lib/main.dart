fc6d5eddda1007242edbd3a3bd07b9badfe262bf
Marcel <marcelcruz@live.com>
2020-05-04 20:41:18 -0300

    Capitalize app name

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Groovr',
       home: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
