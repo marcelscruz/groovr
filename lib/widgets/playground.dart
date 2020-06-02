f66711d1275e9c338b84059b82f1f998d8c7b027
Marcel <marcelcruz@live.com>
2020-06-01 22:22:03 -0300

    Add guitar and notes layers rendering logic

--- lib/widgets/playground.dart
+++ lib/widgets/playground.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
-// import 'package:flare_flutter/flare_actor.dart';
-// import 'package:photo_view/photo_view.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:photo_view/photo_view.dart';
 import 'package:provider/provider.dart';
 import 'package:groovr/models/configuration.dart';
 import 'package:groovr/models/controllers.dart';
 import 'package:groovr/utils/constants.dart';
+
+class PrintLayer extends StatelessWidget {
+  final String image;
+
+  const PrintLayer({@required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      child: SvgPicture.asset(image),
+      top: 20,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    );
+  }
+}
 
 class Playground extends StatefulWidget {
   @override
@@ -14,54 +31,34 @@ class Playground extends StatefulWidget {
 class _PlaygroundState extends State<Playground> {
   String guitarState = 'NONE';
 
-  void changeKey(key) {
-    setState(() {
-      guitarState = key;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Consumer<Controllers>(
       builder: (context, controllers, child) {
         return Consumer<Configuration>(
           builder: (context, configuration, child) {
-            // print(configuration.selectedKey);
-            // print(guitarState);
             return GestureDetector(
               behavior: HitTestBehavior.opaque,
               onTap: () {
                 controllers.closeSlidingPanel();
-
-                // if (guitarState == 'A') {
-                //   changeKey('ASHARP');
-                // } else if (guitarState == 'ASHARP') {
-                //   changeKey('B');
-                // } else {
-                //   changeKey('A');
-                // }
               },
               child: Container(
                 decoration: BoxDecoration(
                   color: kBlack,
                 ),
-                child: Text(
-                  'Guitar',
-                  style: kBody1Bold,
+                child: PhotoView.customChild(
+                  initialScale: 1.5,
+                  minScale: 1.0,
+                  maxScale: 2.0,
+                  basePosition: Alignment.topCenter,
+                  child: Stack(
+                    children: <Widget>[
+                      PrintLayer(image: 'images/guitar.svg'),
+                      PrintLayer(
+                          image: 'images/${configuration.selectedKey}.svg'),
+                    ],
+                  ),
                 ),
-                // child: PhotoView.customChild(
-                //   initialScale: 1.5,
-                //   minScale: 1.0,
-                //   maxScale: 3.0,
-                //   basePosition: Alignment.topCenter,
-                //   child: FlareActor(
-                //     'assets/guitar.flr',
-                //     alignment: Alignment.topCenter,
-                //     fit: BoxFit.cover,
-                //     animation: guitarState,
-                //     // animation: configuration.selectedKey,
-                //   ),
-                // ),
               ),
             );
           },
